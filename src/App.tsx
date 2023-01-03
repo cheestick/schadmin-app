e6679a45c74e8128e379d73279c4a475bd6576ef
Maksym <cheestick@gmail.com>
2023-01-03 14:42:26 +0200

    transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import { Box, Button, VStack, Text, Portal } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  VStack,
+  Text,
+  Portal,
+  Fade,
+  useDisclosure,
+  Slide,
+  SlideFade,
+  Collapse,
+} from '@chakra-ui/react'
 import { useState } from 'react'
 
 const App = () => {
   const [count, setCount] = useState(0)
+  const { isOpen, onToggle } = useDisclosure()
 
   const handleOnClick = () => setCount((prev) => prev + 1)
 
   return (
     <>
       <VStack>
-        <Text>Main App flow</Text>
+        <Text onClick={onToggle}>Main App flow</Text>
       </VStack>
       <Portal>
-        <Box bg="green.400" px="8" py="10" my={10} mx={10} rounded="3xl">
-          <VStack>
-            <Text>Portal</Text>
-            <Text>{count}</Text>
-            <Button onClick={handleOnClick}>Increase</Button>
-          </VStack>
-        </Box>
+        <Collapse in={isOpen} animateOpacity>
+          <Box bg="green.400" px="8" py="10" my={10} mx={10} rounded="3xl">
+            <VStack>
+              <Text>Portal</Text>
+              <Text>{count}</Text>
+              <Button onClick={handleOnClick}>Increase</Button>
+            </VStack>
+          </Box>
+        </Collapse>
       </Portal>
     </>
   )
